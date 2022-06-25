9b5a344ccd44baf468523008ad2517eafaad5e7a
Sebastian Caicedo <scaycedo206@gmail.com>
2022-06-25 01:35:46 -0500

    header home page admin

--- src/Presentation/Ui/Pages/Home-admin/Home-admin.tsx
+++ src/Presentation/Ui/Pages/Home-admin/Home-admin.tsx
@@ -1,5 +1,22 @@
 import React from "react";
-
+import { Header } from "../../Components/Header/Header";
+import "./Home-admin.scss";
+import Doctor from "../../../Common/assets/pose_1-home-admin.png";
+import { Circles } from "../../Components/Circles/Circles";
 export const HomeAdmin = () => {
-  return <div>Home-admin</div>;
+  return (
+    <div className="content-home-admin">
+      <Header />
+      <div className="content-circles-home-admin">
+        <div className="circle-home-admin"></div>
+        <div className="circle-second-home-admin"></div>
+      </div>
+      <img src={Doctor} className="doctor-home-admin" />
+      <div className="background-header-home-admin" />
+      <div className="content-text-header-home-admin">
+        <h1 className="title-home-admin">¡Hola</h1>
+        <h1 className="subtitle-home-admin">Jane Cooper!</h1>
+      </div>
+    </div>
+  );
 };
